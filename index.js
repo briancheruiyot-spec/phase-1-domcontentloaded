53df639db9860b466feba713ea93389db1418eea
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-03-19 09:39:04 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", (e) => {
+  const paragraph = document.querySelector("#text");
+  paragraph.textContent = "This is really cool!"
+})
